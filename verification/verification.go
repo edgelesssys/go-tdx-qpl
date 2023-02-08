b6248c8e1e51f6632bc147a91ba6eb0205119153
Nils Hanke <nils.hanke@outlook.com>
2023-02-08 14:56:13 +0100

    Make fields public

--- verification/verification.go
+++ verification/verification.go
@@ -34,70 +34,70 @@ import (
 */
 
 type SGXQuote4Header struct {
-	version            uint16
-	attestationKeyType uint16
-	teeType            uint32 // 0x0 = SGX, 0x81 = TDX
-	reserved           uint32
-	vendorID           [16]byte
-	userData           [20]byte
+	Version            uint16
+	AttestationKeyType uint16
+	TEEType            uint32 // 0x0 = SGX, 0x81 = TDX
+	Reserved           uint32
+	VendorID           [16]byte
+	UserData           [20]byte
 }
 
 type SGXReport2 struct {
-	tcbSVN         [16]byte
-	mrseam         [48]byte    // SHA384
-	mrsignerseam   [48]byte    // SHA384
-	seamAttributes uint64      // TEE Attributes: In C code that's a [2]uint32
-	tdAttributes   uint64      // TEE Attributes: In C code that's a [2]uint32
-	xfam           uint64      // TEE Attributes: In C code that's a [2]uint32
-	mrtd           [48]byte    // SHA384
-	mrconfigid     [48]byte    // SHA384
-	mrowner        [48]byte    // SHA384
-	mrownerconfig  [48]byte    // SHA384
-	rtmr           [4][48]byte // 4x SHA384 - runtime measurements
-	reportdata     [64]byte    // Likely userData from the original TDREPORT
+	TCBSVN         [16]byte
+	MRSEAM         [48]byte    // SHA384
+	MRSIGNERSEAM   [48]byte    // SHA384
+	SEAMAttributes uint64      // TEE Attributes: In C code that's a [2]uint32
+	TDAttributes   uint64      // TEE Attributes: In C code that's a [2]uint32
+	XFAM           uint64      // TEE Attributes: In C code that's a [2]uint32
+	MRTD           [48]byte    // SHA384
+	MRCONFIG       [48]byte    // SHA384
+	MROWNER        [48]byte    // SHA384
+	MROWNERCONFIG  [48]byte    // SHA384
+	RTMR           [4][48]byte // 4x SHA384 - runtime measurements
+	Reportdata     [64]byte    // Likely UserData from the original TDREPORT
 }
 
 type SGXQuote4 struct {
-	header          SGXQuote4Header
-	body            SGXReport2
-	signatureLength uint32
-	signature       []byte
+	Header          SGXQuote4Header
+	Body            SGXReport2
+	SignatureLength uint32
+	Signature       []byte
 }
 
 func ParseQuote(rawQuote []byte) SGXQuote4 {
 	quoteHeader := SGXQuote4Header{
-		version:            binary.LittleEndian.Uint16(rawQuote[0:2]),
-		attestationKeyType: binary.LittleEndian.Uint16(rawQuote[2:4]),
-		teeType:            binary.LittleEndian.Uint32(rawQuote[4:8]),
-		reserved:           binary.LittleEndian.Uint32(rawQuote[8:12]),
-		vendorID:           [16]byte(rawQuote[12:28]),
-		userData:           [20]byte(rawQuote[28:48]),
+		Version:            binary.LittleEndian.Uint16(rawQuote[0:2]),
+		AttestationKeyType: binary.LittleEndian.Uint16(rawQuote[2:4]),
+		TEEType:            binary.LittleEndian.Uint32(rawQuote[4:8]),
+		Reserved:           binary.LittleEndian.Uint32(rawQuote[8:12]),
+		VendorID:           [16]byte(rawQuote[12:28]),
+		UserData:           [20]byte(rawQuote[28:48]),
 	}
 
 	body := SGXReport2{
-		tcbSVN:         [16]byte(rawQuote[48:64]),
-		mrseam:         [48]byte(rawQuote[64:112]),
-		mrsignerseam:   [48]byte(rawQuote[112:160]),
-		seamAttributes: binary.LittleEndian.Uint64(rawQuote[160:168]),
-		tdAttributes:   binary.LittleEndian.Uint64(rawQuote[168:176]),
-		xfam:           binary.LittleEndian.Uint64(rawQuote[176:184]),
-		mrtd:           [48]byte(rawQuote[184:232]),
-		mrconfigid:     [48]byte(rawQuote[232:280]),
-		mrowner:        [48]byte(rawQuote[280:328]),
-		mrownerconfig:  [48]byte(rawQuote[328:376]),
-		rtmr:           [4][48]byte{[48]byte(rawQuote[376:424]), [48]byte(rawQuote[424:472]), [48]byte(rawQuote[472:520]), [48]byte(rawQuote[520:568])},
-		reportdata:     [64]byte(rawQuote[568:632]),
+		TCBSVN:         [16]byte(rawQuote[48:64]),
+		MRSEAM:         [48]byte(rawQuote[64:112]),
+		MRSIGNERSEAM:   [48]byte(rawQuote[112:160]),
+		SEAMAttributes: binary.LittleEndian.Uint64(rawQuote[160:168]),
+		TDAttributes:   binary.LittleEndian.Uint64(rawQuote[168:176]),
+		XFAM:           binary.LittleEndian.Uint64(rawQuote[176:184]),
+		MRTD:           [48]byte(rawQuote[184:232]),
+		MRCONFIG:       [48]byte(rawQuote[232:280]),
+		MROWNER:        [48]byte(rawQuote[280:328]),
+		MROWNERCONFIG:  [48]byte(rawQuote[328:376]),
+		RTMR:           [4][48]byte{[48]byte(rawQuote[376:424]), [48]byte(rawQuote[424:472]), [48]byte(rawQuote[472:520]), [48]byte(rawQuote[520:568])},
+		Reportdata:     [64]byte(rawQuote[568:632]),
 	}
 
 	// Good job Intel for f***ing up the calculation in the source. The offset here is NOT 656, but 632.
-	// You can reproduce this by seeing that the header is 48 bytes large, and the report body 584 bytes.
+	// You can reproduce this by seeing that the Header is 48 bytes large, and the report Body 584 bytes.
 	// 584 + 48 is 632. That's not 656, Intel! 🤦🏻‍♂️
 	signatureLength := binary.LittleEndian.Uint32(rawQuote[632:636])
 
 	return SGXQuote4{
-		header:          quoteHeader,
-		body:            body,
-		signatureLength: signatureLength,
-		signature:       rawQuote[636:signatureLength],
+		Header:          quoteHeader,
+		Body:            body,
+		SignatureLength: signatureLength,
+		Signature:       rawQuote[636:signatureLength],
 	}
 }
