cead24bbb9b8b64aae2e70a05fe28a6dd9ae22c4
Daniel Weiße <dw@edgeless.systems>
2023-02-21 16:30:41 +0100

    Extend PCS unit tests
    Signed-off-by: Daniel Weiße <dw@edgeless.systems>

--- verification/pcs/pcs_test.go
+++ verification/pcs/pcs_test.go
@@ -3,14 +3,18 @@ package pcs
 import (
 	"context"
 	"crypto/x509"
-	"encoding/pem"
+	"encoding/json"
+	"errors"
 	"fmt"
 	"net/url"
 	"strings"
 	"testing"
+	"time"
 
 	"github.com/edgelesssys/go-tdx-qpl/blobs"
+	"github.com/edgelesssys/go-tdx-qpl/verification/crypto"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 	"go.uber.org/goleak"
 	testclock "k8s.io/utils/clock/testing"
 )
@@ -33,50 +37,169 @@ func TestGetPCKCRL(t *testing.T) {
 }
 
 func TestGetTCBInfo(t *testing.T) {
-	assert := assert.New(t)
-	client := &TrustedServicesClient{
-		api:   &fakeAPI{},
-		clock: testclock.NewFakeClock(blobs.PCSIssueDate),
+	testCases := map[string]struct {
+		api     *fakeAPI
+		time    time.Time
+		wantErr bool
+	}{
+		"success": {
+			api:  &fakeAPI{tcbInfoJSON: blobs.TCBInfoJSON},
+			time: blobs.PCSIssueDate,
+		},
+		"pcs error": {
+			api: &fakeAPI{
+				tcbInfoJSON: blobs.TCBInfoJSON,
+				requestErr:  errors.New("failed"),
+			},
+			time:    blobs.PCSIssueDate,
+			wantErr: true,
+		},
+		"tcb info expired": {
+			api:     &fakeAPI{tcbInfoJSON: blobs.TCBInfoJSON},
+			time:    blobs.PCSIssueDate.Add(24 * 356 * 50 * time.Hour), // 50 years later
+			wantErr: true,
+		},
+		"tcb info not yet valid": {
+			api:     &fakeAPI{tcbInfoJSON: blobs.TCBInfoJSON},
+			time:    time.Time{},
+			wantErr: true,
+		},
+		"tcb info invalid json": {
+			api:     &fakeAPI{tcbInfoJSON: []byte("invalid json")},
+			time:    blobs.PCSIssueDate,
+			wantErr: true,
+		},
+		"tcb info invalid signature": {
+			api: &fakeAPI{tcbInfoJSON: func() []byte {
+				require := require.New(t)
+
+				var tcbInfo struct {
+					TCBInfo   pcsJSONBody `json:"tcbInfo"`
+					Signature string      `json:"signature"`
+				}
+				require.NoError(json.Unmarshal(blobs.TCBInfoJSON, &tcbInfo))
+				tcbInfo.Signature = "00000000000000000000000000000000000000000000000000000000000000001111111111111111111111111111111111111111111111111111111111111111"
+				tcbInfoJSON, err := json.Marshal(tcbInfo)
+				require.NoError(err)
+				return tcbInfoJSON
+			}()},
+			time:    blobs.PCSIssueDate,
+			wantErr: true,
+		},
 	}
 
-	tcbInfo, err := client.GetTCBInfo(context.Background(), [6]byte{0x00, 0x80, 0x6F, 0x05, 0x00, 0x00})
-	assert.NoError(err)
-	assert.NotNil(tcbInfo)
+	for name, tc := range testCases {
+		t.Run(name, func(t *testing.T) {
+			assert := assert.New(t)
+			client := &TrustedServicesClient{
+				api:   tc.api,
+				clock: testclock.NewFakeClock(tc.time),
+			}
+
+			tcbInfo, err := client.GetTCBInfo(context.Background(), [6]byte{0x00, 0x80, 0x6F, 0x05, 0x00, 0x00})
+			if tc.wantErr {
+				assert.Error(err)
+				return
+			}
+
+			assert.NoError(err)
+			assert.NotEmpty(tcbInfo)
+		})
+	}
 }
 
 func TestGetQEIdentity(t *testing.T) {
-	assert := assert.New(t)
-	client := &TrustedServicesClient{
-		api:   &fakeAPI{},
-		clock: testclock.NewFakeClock(blobs.PCSIssueDate),
+	testCases := map[string]struct {
+		api     *fakeAPI
+		time    time.Time
+		wantErr bool
+	}{
+		"success": {
+			api:  &fakeAPI{qeIdentityJSON: blobs.QEIdentityJSON},
+			time: blobs.PCSIssueDate,
+		},
+		"pcs error": {
+			api: &fakeAPI{
+				qeIdentityJSON: blobs.QEIdentityJSON,
+				requestErr:     errors.New("failed"),
+			},
+			time:    blobs.PCSIssueDate,
+			wantErr: true,
+		},
+		"tcb info expired": {
+			api:     &fakeAPI{qeIdentityJSON: blobs.QEIdentityJSON},
+			time:    blobs.PCSIssueDate.Add(24 * 356 * 50 * time.Hour), // 50 years later
+			wantErr: true,
+		},
+		"tcb info not yet valid": {
+			api:     &fakeAPI{qeIdentityJSON: blobs.QEIdentityJSON},
+			time:    time.Time{},
+			wantErr: true,
+		},
+		"tcb info invalid json": {
+			api:     &fakeAPI{qeIdentityJSON: []byte("invalid json")},
+			time:    blobs.PCSIssueDate,
+			wantErr: true,
+		},
+		"tcb info invalid signature": {
+			api: &fakeAPI{qeIdentityJSON: func() []byte {
+				require := require.New(t)
+
+				var qeIdentity struct {
+					TCBInfo   pcsJSONBody `json:"enclaveIdentity"`
+					Signature string      `json:"signature"`
+				}
+				require.NoError(json.Unmarshal(blobs.TCBInfoJSON, &qeIdentity))
+				qeIdentity.Signature = "00000000000000000000000000000000000000000000000000000000000000001111111111111111111111111111111111111111111111111111111111111111"
+				qeIdentityJSON, err := json.Marshal(qeIdentity)
+				require.NoError(err)
+				return qeIdentityJSON
+			}()},
+			time:    blobs.PCSIssueDate,
+			wantErr: true,
+		},
 	}
 
-	qeIdentity, err := client.GetQEIdentity(context.Background())
-	assert.NoError(err)
-	assert.NotNil(qeIdentity)
+	for name, tc := range testCases {
+		t.Run(name, func(t *testing.T) {
+			assert := assert.New(t)
+			client := &TrustedServicesClient{
+				api:   tc.api,
+				clock: testclock.NewFakeClock(tc.time),
+			}
+
+			qeIdentity, err := client.GetQEIdentity(context.Background())
+			if tc.wantErr {
+				assert.Error(err)
+				return
+			}
+			assert.NoError(err)
+			assert.NotNil(qeIdentity)
+		})
+	}
 }
 
-type fakeAPI struct{}
+type fakeAPI struct {
+	tcbInfoJSON    []byte
+	qeIdentityJSON []byte
+	requestErr     error
+}
 
 func (f *fakeAPI) getFromPCS(_ context.Context, uri *url.URL, _ string) ([]byte, *x509.Certificate, error) {
-	signingCertPEM, _ := pem.Decode(blobs.TCBSigningCertPEM)
-	signingCert, err := x509.ParseCertificate(signingCertPEM.Bytes)
-	if err != nil {
-		return nil, nil, err
+	if f.requestErr != nil {
+		return nil, nil, f.requestErr
 	}
-	pckSigningCertPEM, _ := pem.Decode(blobs.CRLSigningCertPEM)
-	pckSigningCert, err := x509.ParseCertificate(pckSigningCertPEM.Bytes)
-	if err != nil {
-		return nil, nil, err
-	}
+
+	signingCert := crypto.MustParsePEMCertificate(blobs.TCBSigningCertPEM)
+	pckSigningCert := crypto.MustParsePEMCertificate(blobs.CRLSigningCertPEM)
 
 	switch {
 	case strings.Contains(uri.Path, pckcrlPath):
 		return blobs.PCKCRLDER(), pckSigningCert, nil
 	case strings.Contains(uri.Path, tcbPath):
-		return blobs.TCBInfoJSON, signingCert, nil
+		return f.tcbInfoJSON, signingCert, nil
 	case strings.Contains(uri.Path, qePath):
-		return blobs.QEIdentityJSON, signingCert, nil
+		return f.qeIdentityJSON, signingCert, nil
 	default:
 		return nil, nil, fmt.Errorf("unexpected path: %s", uri.Path)
 	}
